25a5d3997593ea85330cbf0b63426a1470d599c9
Abidhusain <abidhusain@Abidhusains-MacBook-Air.local>
2023-04-19 00:02:00 +0530

    header setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PartialsModule } from './partials/partials.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    PartialsModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule
