3f8e6ba12fb5abb6f904a801f31ff73aed56d6c3
Abidhusain <abidhusain@Abidhusains-MacBook-Air.local>
2023-04-20 23:12:00 +0530

    daynamic header

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  logoTitle = 'Kesar Creatives';
+  logoSubtitle = 'Create Your Business Software'
+  menuItems = [
+    {
+       label: 'Home',
+       href: '/home',
+    },
+    {
+       subitem:true,
+       label: 'Company',
+       href: '#',
+       children: [
+          {
+            label: 'About Us',
+            href: '/about_us',
+          },
+          {
+            label: 'Privacy Policy',
+            href: '/privacy_policy',
+          },
+          {
+            label: 'Tech Team',
+            href: '/tech_team',
+          },
+          {
+            label: 'Career',
+            href: '/career',
+          }
+        ],
+    },
+    {
+       label: 'Services',
+       href: '/services',
+    },
+    {
+      label: 'www.chrcha.in',
+      href:'#'
+    },
+    {
+      label: 'Blog',
+      href:'#'
+    },
+    {
+      label: 'Contact Us',
+      href:'#'
+    }
+   ];
+   
 
 }
